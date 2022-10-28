c5833fc0f297a8d969486c148a53d93f9992dacf
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2022-10-28 11:09:32 -0500

    adds article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root 'main#index'
+  resources :articles
+  root 'articles#index'
 end
